1ee8018baf47749169832acf696f06392ef029a4
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-22 19:45:48 +0100

    bonus 3: inseriti pulsanti di scroll e start/stop

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/* Consegna:
-
-BONUS 1:
-Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
-BONUS 2:
-Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
-BONUS 3:
-Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. */
-
 const arrowDownHtml = document.querySelector('.fa-chevron-down')
 const arrowUpHtml = document.querySelector('.fa-chevron-up')
 
@@ -46,7 +37,10 @@ const imagesSeeds = [
 
 /*Milestone 1:
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.*/
+Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+BONUS 1:
+Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.*/
 
 for( let i = 0; i < imagesSeeds.length; i++ ){
     if( i === 0 ){
@@ -73,19 +67,64 @@ Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è
 let immagineCorrente = 0
 
 arrowUpHtml.addEventListener( "click", scrollUp)
-
 arrowDownHtml.addEventListener( "click", scrollDown)
 
+/*BONUS 2:
+Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.*/
+let timer = setInterval(scrollDown, 3000)
+let currentScroll = "down"
 
+
+/* Consegna:
+BONUS 3:
+Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. */
+
+const btnPlayPause = document.getElementById("playPause")
+const btnReverse = document.getElementById("reverse")
+
+btnPlayPause.addEventListener("click", function(){
+    if (timer === null) {
+        btnPlayPause.innerHTML="Stop"
+        btnPlayPause.classList.remove("btn-success")
+        btnPlayPause.classList.add("btn-warning")
+
+        if(currentScroll == "down"){
+            timer = setInterval(scrollDown, 3000);
+        } else {
+            timer = setInterval(scrollUp, 3000);
+        }
+      } else {
+        btnPlayPause.innerHTML="Play"
+        btnPlayPause.classList.remove("btn-warning")
+        btnPlayPause.classList.add("btn-success")
+        clearInterval(timer)
+        timer = null
+      }
+})
+
+btnReverse.addEventListener("click", function(){
+    clearInterval(timer)
+    if(currentScroll == "down"){
+        currentScroll = "up"
+        timer = setInterval(scrollUp, 3000)
+    } else {
+        currentScroll = "down"
+        timer = setInterval(scrollDown, 3000)
+    }
+})
+
+// ANCHOR Funzione di scroll verso precedente
 function scrollUp(){
     
     let arrayTagsImmagini = document.querySelectorAll( '#mainImage figure img' )
     let arrayTagsTitoli = document.querySelectorAll( '#mainImage figure h2' )
     let arrayTagsDesc = document.querySelectorAll( '#mainImage figure p' )
+    let arrayTagsThumbnails = document.querySelectorAll( '#thumbnails figure img' )
         
     arrayTagsImmagini[immagineCorrente].classList.remove( 'active' )
     arrayTagsTitoli[immagineCorrente].classList.remove( 'active' )
     arrayTagsDesc[immagineCorrente].classList.remove( 'active' )
+    arrayTagsThumbnails[immagineCorrente].classList.remove( 'active' )
 
     if( immagineCorrente == 0 ){
         immagineCorrente = arrayTagsImmagini.length - 1
@@ -96,18 +135,22 @@ function scrollUp(){
     arrayTagsImmagini[immagineCorrente].classList.add( 'active' )
     arrayTagsTitoli[immagineCorrente].classList.add( 'active' )
     arrayTagsDesc[immagineCorrente].classList.add( 'active' )
-    return
+    arrayTagsThumbnails[immagineCorrente].classList.add( 'active' )
 }
+
+// ANCHOR Funzione di scroll verso successiva
 
 function scrollDown() {
 
     let arrayTagsImmagini = document.querySelectorAll( '#mainImage figure img' )
     let arrayTagsTitoli = document.querySelectorAll( '#mainImage figure h2' )
     let arrayTagsDesc = document.querySelectorAll( '#mainImage figure p' )
+    let arrayTagsThumbnails = document.querySelectorAll( '#thumbnails figure img' )
         
     arrayTagsImmagini[immagineCorrente].classList.remove( 'active' )
     arrayTagsTitoli[immagineCorrente].classList.remove( 'active' )
     arrayTagsDesc[immagineCorrente].classList.remove( 'active' )
+    arrayTagsThumbnails[immagineCorrente].classList.remove( 'active' )
 
     if( immagineCorrente == arrayTagsImmagini.length - 1 ){
         immagineCorrente = 0
@@ -118,5 +161,6 @@ function scrollDown() {
     arrayTagsImmagini[immagineCorrente].classList.add( 'active' )
     arrayTagsTitoli[immagineCorrente].classList.add( 'active' )
     arrayTagsDesc[immagineCorrente].classList.add( 'active' )
+    arrayTagsThumbnails[immagineCorrente].classList.add( 'active' )
 
 }
